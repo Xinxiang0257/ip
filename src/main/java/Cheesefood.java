cd79a7901922c2f0a9bf03b4b25bb9d583c5acb4
HouXinxiang <e1156511@u.nus.edu>
2025-08-30 19:47:49 +0800

    Make the chatbot echo the user and exit when user types bye

--- src/main/java/Cheesefood.java
+++ src/main/java/Cheesefood.java
@@ -1,12 +1,26 @@
+import java.util.Scanner;
+
 public class Cheesefood {
     public static void main(String[] args) {
         String horizontalLine = "____________________________________________________________";
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println(horizontalLine);
         System.out.println(" Hello! I'm Cheesefood");
         System.out.println(" What can I do for you?");
         System.out.println(horizontalLine);
+
+        String userInput;
+        do {
+            userInput = scanner.nextLine();
+            System.out.println(horizontalLine);
+            System.out.println(" " + userInput);
+            System.out.println(horizontalLine);
+        } while (!userInput.equals("bye"));
+
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println(horizontalLine);
+
+        scanner.close();
     }
 }

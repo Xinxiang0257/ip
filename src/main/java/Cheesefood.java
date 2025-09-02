0ae7769a25a383c4057cb2f3d724f5d331d644d2
HouXinxiang <e1156511@u.nus.edu>
2025-09-02 19:31:20 +0800

    Add the ability to store text entered and display them when requested

--- src/main/java/Cheesefood.java
+++ src/main/java/Cheesefood.java
@@ -5,19 +5,39 @@ public class Cheesefood {
         String horizontalLine = "____________________________________________________________";
         Scanner scanner = new Scanner(System.in);
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         System.out.println(horizontalLine);
         System.out.println(" Hello! I'm Cheesefood");
         System.out.println(" What can I do for you?");
         System.out.println(horizontalLine);
 
         String userInput;
-        do {
-            userInput = scanner.nextLine();
-            System.out.println(horizontalLine);
-            System.out.println(" " + userInput);
-            System.out.println(horizontalLine);
-        } while (!userInput.equals("bye"));
+        userInput = scanner.nextLine();
 
+        while (!userInput.equals("bye")){
+            if (userInput.equals("list")) {
+                if (taskCount <= 100) {
+                    System.out.println(horizontalLine);
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    }
+                    System.out.println(horizontalLine);
+                }
+            } else {
+                if (taskCount < 100) {
+                    tasks[taskCount] = userInput;
+                    System.out.println(horizontalLine);
+                    System.out.println(" added: " + userInput);
+                    System.out.println(horizontalLine);
+                    taskCount++;
+                }
+            }
+            userInput = scanner.nextLine();
+        }
+
+        System.out.println(horizontalLine);
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println(horizontalLine);
 
